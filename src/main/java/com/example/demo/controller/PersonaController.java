cb00edc79281473f43903e66a659861fa4da69bf
tesorgroset <tesor@test-angular.com>
2019-07-09 15:42:17 +0200

    Se añaden logs y captura de excepciones

--- src/main/java/com/example/demo/controller/PersonaController.java
+++ src/main/java/com/example/demo/controller/PersonaController.java
@@ -1,16 +1,22 @@
 package com.example.demo.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j2;
+
+
 @RestController
+@Log4j2
 public class PersonaController {
 
 	@GetMapping(value="/personas/anio-matriculacion", consumes=MediaType.TEXT_PLAIN_VALUE, produces=MediaType.TEXT_PLAIN_VALUE)
-	public String calculaAnioMatriculacion(@RequestParam(required=true) String matricula){
-		return "09/05/1978";  
+	public ResponseEntity<String> calculaAnioMatriculacion(@RequestParam(required=true) String matricula){
+		log.debug("devolviendo el año de matriculacion");
+		return ResponseEntity.ok("09/05/1978");  
 	}
 	
 }
---
+++ src/main/java/com/example/demo/exception/DemoExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.example.demo.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class DemoExceptionHandler extends ResponseEntityExceptionHandler{
+	
+
+}
