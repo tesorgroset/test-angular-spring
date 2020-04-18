50484d06899605cbe1d293d50db3480daa209fca
enrique <enrique_arribas@iecisa.com>
2020-04-18 14:36:33 +0200

    Se añade login a la aplicacion

--- src/main/java/com/example/demo/beans/LoginResponse.java
+++ src/main/java/com/example/demo/beans/LoginResponse.java
@@ -4,6 +4,9 @@ import lombok.Data;
 
 @Data
 public class LoginResponse {
+	private long id;
 	private String user;
+	private String firstName;
+	private String lastName;
 	private String jwtToken;
 }
--- src/main/java/com/example/demo/services/LoginServiceImpl.java
+++ src/main/java/com/example/demo/services/LoginServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.example.demo.PersonRepository;
 import com.example.demo.beans.LoginRequest;
 import com.example.demo.beans.LoginResponse;
+import com.example.demo.entity.Person;
 import com.example.demo.exception.UserPasswordIncorrectException;
 import com.example.demo.helpers.JWTHelper;
 
@@ -18,8 +19,12 @@ public class LoginServiceImpl implements LoginService {
 	@Override
 	public LoginResponse login(LoginRequest loginBean) throws UserPasswordIncorrectException {
 		LoginResponse loginResponse = new LoginResponse();
-		if (personRepository.findByUserAndPassword(loginBean.getUser(),loginBean.getPassword()) != null){
-			loginResponse.setUser(loginBean.getUser());
+		Person person = personRepository.findByUserAndPassword(loginBean.getUser(),loginBean.getPassword()); 
+		if ( person != null){
+			loginResponse.setUser(person.getUser());
+			loginResponse.setFirstName(person.getFirstName());
+			loginResponse.setLastName(person.getLastName());
+			loginResponse.setId(person.getId());
 			loginResponse.setJwtToken(JWTHelper.generateJWT(loginBean.getUser()));
 		}else{
 			throw new UserPasswordIncorrectException();
